e271a3bc44e750ac993cf0c2dffec498b99582b0
Taufek Johar <taufek@gmail.com>
2018-03-03 09:18:04 +0800

    Node Js

--- nodejs/entrypoint.sh
+++ nodejs/entrypoint.sh
@@ -1,7 +1,16 @@
 #/bin/sh
 
-cd /var/www/shipping
-php artisan serve --quiet &
+if test -d node_modules;
+then
+  echo node_modules_exists ;
+else
+  cp -a /tmp/node_modules /usr/src/app/website;
+fi
+
+npm install
+
+# cd /var/www/shipping
+# php artisan serve --quiet &
 
 cd /var/www/frontend
 npm start
